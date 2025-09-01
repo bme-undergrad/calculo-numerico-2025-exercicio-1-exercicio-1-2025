f62753424b830525552538d8a1067e94e1d909a5
Bruno Bellini <69762038+ChunoB@users.noreply.github.com>
2025-09-01 11:50:05 -0300

    Update exercicio1.m
    exercício alterado

--- exercicio1.m
+++ exercicio1.m
@@ -1,15 +1,34 @@
-function t = exercicio1(func,x0)
+function t = exercicio1(func,func_d,x0)
 
 % nao alterar: inicio
-es = 1;
+es = 0.01;
 imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
-t = 0; % alterar
+v = zeros(imax,1);     
+v(1) = x0;             
+erro = zeros(imax,1);  
+
+printf("Iteração | Aproximação     | Erro Relativo\n");
+printf("-------------------------------------------\n");
+
+for ii = 1:imax-1
+  if ii ~= 1
+    erro(ii) = abs((v(ii)-v(ii-1))/v(ii));
+    printf("%3d      | %12.6f   | %12.6f\n", ii, v(ii), erro(ii));
+    if erro(ii) < es
+      break;
+    endif
+  else
+    printf("%3d      | %12.6f   |    -----------\n", ii, v(ii));
+  endif
+  v(ii+1) = v(ii) - func(v(ii))/func_d(v(ii));
+endfor
+
+t = v(ii);   % último valor é a raiz
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-
 endfunction
+
